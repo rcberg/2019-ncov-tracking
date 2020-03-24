1af23812bebd44474010b2809fb90b1eceb4eb69
Chris Berg <cberg@uoregon.edu>
2020-03-23 22:46:53 -0700

    looks like JHU changed their data format... again...

--- data/data_update.R
+++ data/data_update.R
@@ -20,7 +20,7 @@ pacman::p_load( tidyverse ,
 ## confirmed cases
 
 raw_confirmed_data = 
-  read_csv("https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Confirmed.csv")
+  read_csv("https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv")
 
 confirmed_data_df = 
   raw_confirmed_data  %>%
@@ -32,7 +32,7 @@ confirmed_data_df =
 
 ## death cases
 
-raw_deaths_data = read_csv("https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Deaths.csv")
+raw_deaths_data = read_csv("https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_global.csv")
 
 
 deaths_data_df = 
@@ -43,25 +43,12 @@ deaths_data_df =
   mutate( type = "Death" , 
           date = mdy(date) )
 
-## recovery cases
-
-raw_recov_data = read_csv("https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Recovered.csv")
-
-
-recov_data_df = 
-  raw_recov_data  %>%
-  pivot_longer( -c("Province/State":"Long") , 
-                names_to = "date" , 
-                values_to = "cases" ) %>%
-  mutate( type = "Recovered" , 
-          date = mdy(date) )
-
 ## grand dataset 
 
 data_df = 
   confirmed_data_df %>%
-  bind_rows( deaths_data_df , 
-             recov_data_df ) %>% 
+  bind_rows( deaths_data_df
+             ) %>% 
   clean_names() %>%
   mutate(province_state = replace_na(province_state , "Nationwide") ) %>%
   arrange( country_region ,
@@ -71,65 +58,10 @@ saveRDS( data_df , "data/export/worldwide_data.rds" )
 
 ## united states datasets
 
-# us state data
-
-state_data_df =
-  data_df %>% 
-  filter( country_region == "US" )
-
-saveRDS( state_data_df , "data/export/usa_state_data.rds" )
-
-# us local data
-
-usa_localities_later_df = 
-  state_data_df %>% 
-  filter( province_state %in% c(state.name, "Washington, D.C." , "Diamond Princess" , "Grand Princess") &
-            date > "2020-03-09") %>% select(-country_region )
-
-coordinates = 
-  usa_localities_later_df %>%
-  filter( date == "2020-03-22" ) %>%
-  select( province_state , 
-          lat , 
-          long) %>% distinct()
-
-usa_localities_early_df = 
-  state_data_df %>% 
-  filter( !province_state %in% state.name &
-            date < "2020-03-10") %>%
-  separate( col = "province_state" , sep = ", ", into = c(NA , "abb")) %>%
-  merge( state_name_abb ) %>% 
-  select(-abb) %>%
-  bind_rows(state_data_df %>% 
-              filter( province_state %in% c("Diamond Princess" , "Grand Princess" , "District of Columbia") &
-                        date < "2020-03-10") ) %>%
-  group_by( province_state , date , type ) %>%
-  summarise( cases = sum(cases) ) %>% left_join( coordinates )
-
-usa_local_df = bind_rows( usa_localities_early_df , usa_localities_later_df)
-
-saveRDS( usa_local_df , "data/export/usa_statewide_data.rds")
-
-
 # us nationwide data
 
-usa_early2020_df = 
-  state_data_df %>% 
-  filter( !province_state %in% state.name &
-          date < "2020-03-10") %>%
-  group_by( date , type ) %>%
-  summarise( cases = sum(cases) ) %>%
-  arrange( desc(date))  
-
-usa_data_mmar_df = 
-  state_data_df %>% 
-  filter( province_state %in% c(state.name, "Washington, D.C." , "Diamond Princess" , "Grand Princess") &
-            date > "2020-03-09") %>%
-  group_by( date , type ) %>%
-  summarise( cases = sum(cases) ) %>%
-  arrange( desc(date))
-
 usa_data_df = 
-  rbind(usa_early2020_df , usa_data_mmar_df)
+  data_df %>% 
+  filter( country_region == "US" )
 
 saveRDS( usa_data_df , "data/export/usa_national_data.rds" )
